f8c853b2803f65d67f165b394831d401f195dcb0
ozubergz <jmin499@gmail.com>
2020-11-19 14:50:24 -0500

    collections and iteration

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,43 @@
-//fun sayHello(itemToGreet:String) {
-//    println("Hello $itemToGreet")
-//}
-
-//single line expression
-fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet")
+fun sayHello(greeting:String, things:List<String>) {
+    things.forEach{ thing ->
+        println("$greeting $thing")
+    }
+}
 
 fun main() {
-    sayHello("Hey", "Kotlin")
-    sayHello("Hello", "World")
+//    val things = arrayOf("Kotlin", "Programming", "Comic Books")
+    val things = listOf("Kotlin", "Programming", "Comic Books")
+    sayHello("Hi", things)
+
+//    val things = mutableListOf("Kotlin", "Programming", "Comic Books")
+//    things.add("Dogs")
+//
+//    val map = mutableMapOf(1 to "a", 2 to "b", 3 to "c")
+//    map.put(4, "d")
+//    println(map)
+
+//    val map = mapOf(1 to "a", 2 to "b", 3 to "c")
+//
+//    map.forEach { key, value -> println("$key -> $value") }
+
+//    println(things.size)
+//    println(things[0])
+//    println(things.get(0))
+
+//    for(thing in things) {
+//        println(thing)
+//    }
+
+//    things.forEach {
+//        println(it)
+//    }
+
+//    things.forEach { thing ->
+//        println(thing)
+//    }
+
+//    things.forEachIndexed { index, thing ->
+//        println("$thing is at index $index")
+//    }
+
 }
