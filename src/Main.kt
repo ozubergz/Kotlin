ba2ddadec4cabb9a527cdaa1c454c52a1b2cd169
ozubergz <jmin499@gmail.com>
2020-11-20 15:04:58 -0500

    named arg & default values

--- src/Main.kt
+++ src/Main.kt
@@ -1,43 +1,24 @@
-fun sayHello(greeting:String, things:List<String>) {
-    for(thing in things) {
-        println(thing)
+fun sayHello(greeting:String, vararg things:String) {
+    things.forEach { thing ->
+        println("$greeting $thing")
     }
 }
 
-//fun sayHello(greeting:String, things:List<String>) {
-//    things.forEach{ thing ->
-//        println("$greeting $thing")
-//    }
-//}
-
-//fun sayHello(greeting:String, things:List<String>) {
-//    things.forEachIndexed { index, thing ->
-//        println("$thing is at index $index")
-//    }
-//}
-
-//fun sayHello(greeting:String, map: Map<Int, String>) {
-//    map.forEach { key, value -> println("$key -> $value") }
-//}
+fun greetPerson(greet:String, name:String) = println("$greet $name")
 
 fun main() {
-//    val things = arrayOf("Kotlin", "Programming", "Comic Books")
-    val things = listOf("Kotlin", "Programming", "Comic Books")
+//    val things = listOf("Kotlin", "Programming", "Comic Books")
+     val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
 
-//    val things = mutableListOf("Kotlin", "Programming", "Comic Books")
-//    things.add("Dogs")
+//    sayHello("Hi", "Kotlin", "Programming", "Comic Books")
+//    sayHello("Hi", *interestingThings)
 
-//    println(things.size)
-//    println(things[0])
-//    println(things.get(0))
+//  pass arguments in order
+    greetPerson("Hi", "Kotlin")
 
-    sayHello("Hi", things)
+//  pass named arguments
+    greetPerson(name = "World", greet = "Hello")
 
-//    val map = mapOf(1 to "a", 2 to "b", 3 to "c")
-
-//    val map = mutableMapOf(1 to "a", 2 to "b", 3 to "c")
-//    map.put(4, "d")
-//    println(map)
-
-//    sayHello("Hi", map)
+//  pass named arguments
+    sayHello(things = *interestingThings, greeting = "Hi")
 }
