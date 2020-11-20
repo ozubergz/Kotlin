54929a0f6cc296d31d5c52f63da934fa62f4d86b
ozubergz <jmin499@gmail.com>
2020-11-20 15:54:39 -0500

    classees

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,12 @@
-fun sayHello(greeting:String, vararg things:String) {
-    things.forEach { thing ->
-        println("$greeting $thing")
-    }
-}
+//fun sayHello(greeting:String, vararg things:String) {
+//    things.forEach { thing ->
+//        println("$greeting $thing")
+//    }
+//}
 
-fun greetPerson(greet:String, name:String) = println("$greet $name")
+//fun greetPerson(greet:String, name:String) = println("$greet $name")
 
 fun main() {
-//    val things = listOf("Kotlin", "Programming", "Comic Books")
-     val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
-
-//    sayHello("Hi", "Kotlin", "Programming", "Comic Books")
-//    sayHello("Hi", *interestingThings)
-
-//  pass arguments in order
-    greetPerson("Hi", "Kotlin")
-
-//  pass named arguments
-    greetPerson(name = "World", greet = "Hello")
-
-//  pass named arguments
-    sayHello(things = *interestingThings, greeting = "Hi")
+    val person = Person("John", "Doe")
+    person.printInfo()
 }
---
+++ src/Person.kt
@@ -0,0 +1,47 @@
+class Person(val firstName: String = "Peter", val lastName: String = "Parker") {
+
+    //you can give property  non-visibility by typing private or protected before var
+    var nickName: String? = null
+        // setter
+        set(value) {
+            field = value
+            println("set nickname is $value")
+        }
+
+        // getter
+        get() {
+            println("get nickname, $field")
+            return field
+        }
+
+        //method
+        fun printInfo() {
+//            val nickNameToPrint = if(nickName != null) nickName else "no nickname"
+            val nickNameToPrint = nickName ?: "no nickname"
+
+            println("$firstName ($nickNameToPrint) $lastName")
+        }
+
+    //initializing properties to this class
+    //But, you can initialize properties inside the parameter
+
+//    val firstName: String = _firstName
+//    val lastName: String = _lastName
+
+    // initializer, but it's not required as long it's been defined and initialized at the top
+    // init block will always run first before constructor
+
+//    init {
+//        firstName = _firstName
+//        lastName = _lastName
+//    }
+
+    //default values; this is the secondary constructor
+    //secondary constructor is not required as long as default values is assigned in the parameters
+
+//    constructor(): this("Peter", "Parker") {
+//        println("secondary constructor")
+//    }
+
+
+}
